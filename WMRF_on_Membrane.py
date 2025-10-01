30577d1ae5679facc49987c705179ffbd1b4a15f
David-SUSTech <2831529479@qq.com>
2025-10-01 23:49:15 +0800

    Update and rename WMRF on Membrane to WMRF_on_Membrane.py

--- WMRF_on_Membrane.py
+++ WMRF_on_Membrane.py
@@ -1,5 +1,5 @@
 # This code is about WMRF (Wasserstein Markov Random Forest)
-'''python
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
